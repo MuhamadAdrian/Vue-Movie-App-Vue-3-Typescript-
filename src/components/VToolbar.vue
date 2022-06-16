a0239dcec681284936d18c43b75701e1710ba870
MuhamadAdrian <madrian.septiana@gmail.com>
2022-06-16 11:31:15 +0700

    [fix] not showing number of bookmarked items on zero

--- src/components/VToolbar.vue
+++ src/components/VToolbar.vue
@@ -2,7 +2,9 @@
     <div class="flex items-center gap-x-5">
         <VButton class="ring-gray-600 rounded-full p-1" icon="history" stroke="currentColor" />
         <VButton class="ring-gray-600 rounded-full p-1 relative" icon="bookmark" stroke="currentColor">
-            <VBadges class="-top-1.5 -right-1.5 bg-white text-gray-900" :text="store.state.bookmark.bookmark.length" />
+            <Transition name="bounce" mode="out-in">
+                <VBadges v-if="store.state.bookmark.bookmark.length > 0" class="-top-1.5 -right-1.5 bg-white text-gray-900" :text="store.state.bookmark.bookmark.length" />
+            </Transition>
         </VButton>
         <VButton class="ring-gray-600 rounded-full p-1" icon="search" stroke="currentColor" />
     </div>
